617bc67669357e6c5a2150638ce790290112b935
Masanari Miyazaki <masanari.a.miyazaki@git.rakuten-it.com>
2018-07-05 17:22:08 +0900

    made table.

--- pages/price.vue
+++ pages/price.vue
@@ -1,11 +1,32 @@
 <template>
   <section class="container">
-	iii
+	個人だから実現できる、他社のサービスより圧倒的にお得な価格設定
+	英語学習サポートサービスを提供している会社の半分以下の価格で同様のサービスを提供いたします。
+	他社のサービスも、第二言語学習論という研究結果に基づいた学習カリキュラムを提供しており、自身での英語学習の経験と実績があれば
+	個人でも十分に科学的に効率的な学習カリキュラムを提供できると自負しております。
+	
 	<table class="brwsr2">
 	  <tbody>
 		<tr>
-		  <th>FireFox</th>
+		  <th class="header-left">会社名</th>
+		  <th class="data fst">
+			月額料金
+		  </th>
+		  <th class="data">
+			入会金
+		  </th>
+		  <th class="data">
+			レッスン数
+		  </th>
+		  <th class="data">
+			受講期間
+		  </th>
+		</tr>
+		<tr>
 		  <td class="data fst">
+			タブ型
+		  </td>
+		  <td class="data">
 			タブ型
 		  </td>
 		  <td class="data">
@@ -17,15 +38,14 @@
 		  <td class="data">
 			89言語対応
 		  </td>
-		  <td class="data">
-			Mozilla
-		  </td>
 		</tr>
 		<tr>
 		  <td class="bar" colspan="6"></td>
 		</tr>
 		<tr>
-		  <th>Chrome</th>
+		  <td class="data fst">
+			タブ型
+		  </td>
 		  <td class="data fst">
 			タブ型
 		  </td>
@@ -38,15 +58,14 @@
 		  <td class="data">
 			53言語対応
 		  </td>
-		  <td class="data">
-			Google
-		  </td>
 		</tr>
 		<tr>
 		  <td class="bar" colspan="6"></td>
 		</tr>
 		<tr>
-		  <th>IE</th>
+		  <td class="data fst">
+			7以降タブ型
+		  </td>
 		  <td class="data fst">
 			7以降タブ型
 		  </td>
@@ -58,9 +77,6 @@
 		  </td>
 		  <td class="data">
 			95言語対応
-		  </td>
-		  <td class="data">
-			Microsoft
 		  </td>
 		</tr>
 	  </tbody>
@@ -79,25 +95,30 @@ export default {
 </script>
 
 <style>
+.header-left {
+  border-left: 1px solid whitesmoke;
+}
+
 table.brwsr2 {
   margin: 0 auto;
   width: 650px;
   border: none;
-  border-top: solid 1px #666;
+  border-top: solid 1px whitesmoke;
   border-bottom: solid 1px #666;
   border-collapse: separate;
-  border-spacing: 0 10px;
-  background: #f5f5f5;
+  border-spacing: 0px;
+  background: white;
+  border-radius: 5px;
 }
 
 table.brwsr2 th {
   vertical-align: middle;
   height: 53px;
   width: 124px;
-  border-right: solid 1px #666;
+  border-right: solid 1px whitesmoke;
   margin: 0;
   text-align: center;
-  color: #333;
+  color: #3a3b38;
   font-size: 16px;
   font-weight: bold;
 }
@@ -125,6 +146,6 @@ table.brwsr2 td:fst-child {
 table.brwsr2 td.bar {
   height: 1px;
   width: 100%;
-  border-top: solid 1px #34495e;
+  border-top: solid 0.5px whitesmoke;
 }
 </style>
---
+++ pages/review.vue
@@ -0,0 +1,72 @@
+<template>
+  <section class="container">
+	受講者はまだいません。初回限定キャンペーンとして、半額で提供させていただいています。
+  </section>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      form: {
+        email: '',
+        name: '',
+        food: null,
+        checked: []
+      },
+      foods: [
+        { text: 'Select One', value: null },
+        'Carrots', 'Beans', 'Tomatoes', 'Corn'
+      ],
+      show: true
+    }
+  },
+  methods: {
+    onSubmit (evt) {
+      evt.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+    onReset (evt) {
+      evt.preventDefault();
+      /* Reset our form values */
+      this.form.email = '';
+      this.form.name = '';
+      this.form.checked = [];
+      /* Trick to reset/clear native browser form validation state */
+      this.show = false;
+      this.$nextTick(() => { this.show = true });
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
