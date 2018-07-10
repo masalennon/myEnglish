5fe9134fc5584656a5634aebbd848b30e3ca6a7c
Masanari Miyazaki <masanari.a.miyazaki@git.rakuten-it.com>
2018-07-10 16:56:17 +0900

    made content page responsive

--- pages/content.vue
+++ pages/content.vue
@@ -27,20 +27,21 @@
 
   <main class="main-container">
 	<article>
-	  <div class="make-carriculum">
-		<h1 class="make-carriculum-h1">
-		  一人一人のレベル、目標にあった<br>
-		  学習カリキュラムを作成。
-		</h1>
-		<!--<div class="both-ok">-->
-		  <!--<img :src="require('../assets/both-OK.png')" />-->
-		  <!--</div>-->
-		<p class="make-carriculum-paragraph">
-		  TOEIC, 日常会話のマスターなどお客様の要望に合わせた学習計画を立て、継続して実行していけるようにサポートしていきます。<br>
-		  初回のカウンセリングで現状の英語力と目標、英語を学びたい理由などを伺い、<br>
-		  最適な学習方法とその科学的根拠、市販の教材を用いた具体的な学習方法を提案いたします。<br>
-		  まず初めに学ぶ理由、なぜその学習方法が効率的であるかについての科学的根拠をはっきりさせるので、納得して学習を続けていただけます。<br>
-		</p>
+	  <div class="row">
+		<div class="col-md-12">
+		  <h1 class="">
+			一人一人に学習カリキュラムを作成。<br>
+		  </h1>
+		  <!--<div class="both-ok">-->
+			<!--<img :src="require('../assets/both-OK.png')" />-->
+			<!--</div>-->
+		  <p class="make-carriculum-paragraph">
+			TOEIC, 日常会話のマスターなどお客様の要望に合わせた学習計画を立て、継続して実行していけるようにサポートしていきます。
+			初回のカウンセリングで現状の英語力と目標、英語を学びたい理由などを伺い、
+			最適な学習方法とその科学的根拠、市販の教材を用いた具体的な学習方法を提案いたします。
+			まず初めに学ぶ理由、なぜその学習方法が効率的であるかについての科学的根拠をはっきりさせるので、納得して学習を続けていただけます。
+		  </p>
+		</div>
 	  </div>
 
 	  <div class="life-advice">
@@ -48,102 +49,86 @@
 		  学習を続けていくために<br>
 		</h1>
 
-		<div class="life-advice-row">
-		  <div class="life-advice1">
+		<div class="row">
+		  <div class="col-md-4">
 			<img style="height: 150px;" :src="require('../assets/icon.svg')" />
 			<h4>
 			  Life
 			</h4>
 			<p class="life-advice-paragraph1">
 			  働きながら学習時間をどうしたら捻出できるか、自分を律して学習を毎日続けて行くにはどうしたらいいかについてもアドバイス。
-			  心理学や科学の研究を基にした的確な
+			  心理学の研究結果を基に、意思の力を高め目標を達成するためのマインドセットの作り方までお伝えします。
 			</p>
 		  </div>
-		  <div class="life-advice1">
+		  <div class="col-md-4">
 			<img style="height: 150px;" :src="require('../assets/calendar.svg')" />
 			<h4>
 			  Schedule
 			</h4>
 			<p class="life-advice-paragraph2">
-			  英語学習についてのアドバイスだけではなく、働きながら学習時間をどうしたら捻出できるか、自分を律して学習を毎日続けて行くにはどうしたらいいかなどの
-			  自己管理面や生活習慣についても個人の経験や心理学の知識を応用しながら的確にアドバイスいたします。
-			  毎日2~3時間の学習時間を設けることは働きながらではすごく大変です。しかし、スキマ時間でできる学習や時間の捻出方法についても一緒に考え、
-			  アドバイスいたしますので、継続して学習をしていただけます。
+			  契約期間中迷いなく学習を進めていただけるように、あなた専用の学習スケジュールをお作りします。個人の好みを伺い、使用する
+			  参考書を一緒に選ぶところから目標達成のためにそれをどう活用していくかまでサポートします。
 			</p>
 		  </div>
-		  <div class="life-advice1">
+		  <div class="col-md-4">
 			<img style="height: 150px;" :src="require('../assets/homework.svg')" />
 			<h4>
 			  Homework
 			</h4>
 			<p class="life-advice-paragraph3">
-			  英語学習についてのアドバイスだけではなく、働きながら学習時間をどうしたら捻出できるか、自分を律して学習を毎日続けて行くにはどうしたらいいかなどの
-			  自己管理面や生活習慣についても個人の経験や心理学の知識を応用しながら的確にアドバイスいたします。
-			  毎日2~3時間の学習時間を設けることは働きながらではすごく大変です。しかし、スキマ時間でできる学習や時間の捻出方法についても一緒に考え、
-			  アドバイスいたしますので、継続して学習をしていただけます。
+			  継続して毎日の学習を行えるように、宿題の設定をいたします。宿題を全てこなしていけばあなたの目標を達成できるように作成しますので、
+			  毎日の学習のペースメーカーとしてお役立てください。もちろん、自分で学ぶペースを決めたい、など希望がありましたら柔軟にお答えいたします。
 			</p>
 		  </div>
 		</div>
 	  </div>
 
 
-	  <div class="online-support">
-		<h1 class="online-support-h1">
-		  毎日の学習を支える、<br>
-		  オンラインサポート。
+	  <div class="">
+		<h1 class="">
+		  毎日の学習を支えるオンラインサポート。
 		</h1>
-		<div class="life-advice-row">
-		  <div class="online-support1">
+		<div class="row">
+		  <div class="col-md-6">
 			<img style="height: 150px;" :src="require('../assets/smartphone.svg')" />
 			<h4>
 			  チャットで気軽に質問
 			</h4>
-			<p class="online-support-paragraph1">>
-			  ボイスメッセージを使ったシャドーウィング、オーバーラッピング、音読の添削や学習に関する悩み相談など、週5日チャットツールでサポートいたします。<br>
-			  お送りいただいたメッセージには基本的に24時間以内に返信をします。毎日の学習のリズムを整えるのにお役立てください。<br>
-
-			  満足いただけなかった場合は、日計算で残りの代金を全額返済
-			  英語だけでなく全てに共通する独学の技術
-
-			  自己管理能力、やりきる力、独学のコツ、心理学に沿ったモチベーションの維持の仕方
-			  単語が覚えられない、文法がわからない、英語が聴き取れない、英語が口から出てこない、英語を読んでも頭に入ってこない、これらのお悩みを解消するための
-			  勉強方法をお教えします。
+			<p class="online-support-paragraph1">
+			  ボイスメッセージを使ったシャドーウィング、オーバーラッピング、音読の添削や学習に関する悩み相談など、平日週5日チャットツールでサポートいたします。<br>
+			  毎日の学習のリズムを整えるのにお役立てください。<br>
 			</p>
 		  </div>
-		  <div class="online-support1">
+		  <div class="col-md-6">
 			<img style="height: 150px;" :src="require('../assets/delivery.svg')" />
 			<h4>
 			  24時間以内の素早い対応
 			</h4>
-			<p class="online-support-paragraph1">>
-			  ボイスメッセージを使ったシャドーウィング、オーバーラッピング、音読の添削や学習に関する悩み相談など、週5日チャットツールでサポートいたします。<br>
-			  お送りいただいたメッセージには基本的に24時間以内に返信をします。毎日の学習のリズムを整えるのにお役立てください。<br>
-
-			  満足いただけなかった場合は、日計算で残りの代金を全額返済
-			  英語だけでなく全てに共通する独学の技術
-
-			  自己管理能力、やりきる力、独学のコツ、心理学に沿ったモチベーションの維持の仕方
-			  単語が覚えられない、文法がわからない、英語が聴き取れない、英語が口から出1¦こない、英語を読んでも頭に入ってこない、これらのお悩みを解消するための
-			  勉強方法をお教えします。
+			<p class="online-support-paragraph1">
+			  お送りいただいたメッセージには基本的に24時間以内に返信をします。
+			  その他にも、単語が覚えられない、文法がわからない、英語が聴き取れない、英語が口から出てこない、英語を読んでも頭に入ってこないなど、
+			  毎日の学習の中で出てくるあなたのお悩みに毎日お答えします。<br>
 			</p>
 		  </div>
 		</div>
 	  </div>
 
 
-	  <div class="unnecessary-eikaiwa">
-		<h1>
-		  もう英会話教室は<br>
-		  必要ありません！
-		</h1>
-		<p class="unnecessary-eikaiwa-paragraph">
-		  良質な教材はネット、書店に溢れています。<br>
-		  必要なのはそれらの正しい活用方法と継続力です。<br>
-		  まずはお気軽に、無料カウンセリングをお試しください!<br>
-		  <router-link to="/apply">
-			お申し込みはこちら
-		  </router-link>
-	  </p>
+	  <div class="row">
+		<div class="col-md-12 unnecessary-eikaiwa">
+		  <h1>
+			もう英会話教室は<br>
+			必要ありません！
+		  </h1>
+		  <p class="unnecessary-eikaiwa-paragraph">
+			良質な教材はネット、書店に溢れています。<br>
+			必要なのはそれらの正しい活用方法と独学力です。<br>
+			まずはお気軽に、無料カウンセリングをお試しください!<br>
+			<router-link to="/apply">
+			  お申し込みはこちら
+			</router-link>
+		  </p>
+		</div>
 	</div>
   </article>
 </main>
@@ -155,8 +140,11 @@
 <style lang="scss">
 @import '../src/assets/scss/app.scss';
 h1 {
-  height: 80px;
   color: #5A5AD2;
+  width: 80%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 30px;
   font-weight: 700;
 }
 
@@ -170,6 +158,7 @@ h1 {
 }
 
 .make-carriculum-paragraph {
+  width: 80% !important;
 }
 
 .life-advice {
@@ -185,6 +174,7 @@ h1 {
 
 .online-support {
   margin-bottom: 60px;
+  height: 700px;
 }
 
 .online-support-paragraph {
@@ -207,12 +197,12 @@ h1 {
 }
 
 
-.make-carriculum .make-carriculum-paragraph p {
-
+.make-carriculum {
+  padding-bottom: 50px;
 }
 
 .life-advice-row {
-  height: 800px !important;
+  height: 700px !important;
   width: 80% !important;
   margin-right: auto;
   margin-left: auto;
