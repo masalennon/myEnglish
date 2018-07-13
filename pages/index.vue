436f06dd974175efdf4818c80537fe9e8a69cf79
Masanari Miyazaki <masanari.a.miyazaki@git.rakuten-it.com>
2018-07-13 16:59:30 +0900

    modified index page mainly.

--- pages/index.vue
+++ pages/index.vue
@@ -1,47 +1,74 @@
 <template>
   <main class="container" id="index">
-	<div class="">
-	  <div class="first-section">
-		<h1>
-		  今度こそ本気で英語を身につけたいあなたへ
-		</h1>
-		<h3>
-		  <p>
-			英語の勉強をしてはいるものの、なかなか成果が出ない方
-		  </p>
-		  <p>
-			英会話教室に通ったがなかなか身につかないまま費用が高くてやめてしまった方
-		  </p>
-		  <p>
-			英会話スクールと違って受け身にならない
-		  </p>
-		  <p>
-			英語の勉強を始めても何回も挫折してしまう方
-		  </p>
-		  <p>
-			TOEICでは高得点を獲得できたものの、<br>
-			思ったように英語が話せず悩んでいる方
-		  </p>
-		</h3>
+	<div>
+	  <div class="row">
+		<section class="first-section">
+		  <h1>
+			今度こそ本気で英語を身につけたいあなたへ
+		  </h1>
+		  <div class="first-list" style="">
+			あなたには、こんな経験はないでしょうか？
+			<ul>
+			  <li>
+				英語の勉強をしてはいるものの、なかなか成果が出ない
+			  </li>
+			  <li>
+				英会話教室に通ったがなかなか身につかないまま費用が高くてやめてしまった
+			  </li>
+			  <li>
+				英語の勉強を始めても毎回挫折してしまう
+			  </li>
+			  <li>
+				TOEICでは高得点を獲得できたものの、うまく話せず悩んでいる
+			  </li>
+			</ul>
+		  </div>
+		</section>
+		<div class="row">
+		  <section>
+			3コースがお選びいただけます
+			基礎コース
+			TOEIC対策コース
+			英会話コース
+		  </section>
+		</div>
 	  </div>
 
-	  <div class="second-section">
-		<h2>
-		  科学に裏打ちされた学習カリキュラムだから効率的に<br>
-		  個人によるマンツーマンレッスンだからお手頃に<br>
-		  学習のサポートが充実しているから継続的に<br>
-		  限界を目標に設定するからストイックに<br>
-		  自習サポート型だから能動的に<br>
-		  英語を学べます。
-		</h2>
-
-		独学で英語を身につけた講師が親身になってサポート
-		あなたが目標を達成するまで、最後までサポートします。
-		圧倒的な低価格
-		英語だけでなく全てに共通する独学のスキルの習得
-
-		下にスクロールしていくと植物が育っていくようにしよう。
-
+	  <div class="row">
+		<section class="second-section">
+		  <div class="second-list">
+			<ul>
+			  <li class="item">
+				科学に裏打ちされた学習カリキュラムだから<em>効率的</em>
+			  </li>
+			  <li class="item">
+				<div class="sentence">
+				  参考書選びから使い方までサポートするから<em>迷わない</em>
+				</div>
+			  </li>
+			  <li class="item">
+				<div class="sentence">
+				  個人によるマンツーマンレッスンだから<em>お手頃</em>
+				</div>
+			  </li>
+			  <li class="item">
+				<div class="sentence">
+				  継続するためのサポートが充実しているから<em>継続できる</em>
+				</div>
+			  </li>
+			  <li class="item">
+				<div class="sentence">
+				  あなたの限界を目標に設定するから<em>短期集中的</em>
+				</div>
+			  </li>
+			  <li class="item">
+				<div class="sentence">
+				  自習サポート型だから受け身にならず<em>能動的</em>
+				</div>
+			  </li>
+			</ul>
+		  </div>
+		</section>
 	  </div>
 
 	</div>
@@ -50,28 +77,93 @@
 </template>
 
 <style lang="scss">
-.container {
-  min-height: 100vh;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  max-width: 100%;
-  p {
-	margin-bottom: 0px !important;
-  }
-  h3 {
-	margin-bottom: 0px;
-  }
-  .first-section {
-	background: linear-gradient(to bottom, #fff 0, #f6f6f6 150px);
-  }
-  .second-section {
-	background: linear-gradient(to top, #fff 0, #f6f6f6 150px);
-  }
-  .links {
-	padding-top: 15px;
-  }
-}
+  html {
+	background: whitesmoke !important;
+	.copyright{
+	  background: whitesmoke;
+	  margin-top: 0px;
+	}
+	.container {
+	  background: whitesmoke;
+	  min-height: 100vh;
+	  justify-content: center;
+	  align-items: center;
+	  text-align: center;
+	  max-width: 100%;
+	  p {
+		margin-bottom: 0px !important;
+	  }
+	  h3 {
+		margin-bottom: 0px;
+	  }
+	  .first-section {
+		background: white;
+		width: 80%;
+		margin-left: auto;
+		margin-right: auto;
+		.first-list {
+		  width: 70%;
+		  margin-left: auto;
+		  margin-right: auto;
+		  li {
+			text-align: left;
+			list-style: none;
+		  }
+		}
+	  }
+	  .second-section {
+		margin-left: auto;
+		margin-right: auto;
+		background: white;
+		width: 80%;
+		.second-list{
+		  width: 80%;
+		  margin-left: auto;
+		  margin-right: auto;
+		  ul {
+			padding-top: 30px;
+			li {
+			  text-align: left;
+			  height: 80px;
+			  padding: 0.1em 1rem 0em 10%;
+			  font-size: 1.8rem;
+			  list-style: none;
+			  background-image: url(../assets/checkmark.svg);
+			  background-repeat: no-repeat;
+			  border-bottom: whitesmoke solid thick;
+			  margin-top: 20px;
+			  .item {
+				vertical-align: bottom;
+			  }
+			  em {
+				color: #ea8b00;
+				font-weight: 700;
+			  }
+			}
+		  }
+		}
+	  }
+	  .links {
+		padding-top: 15px;
+	  }
 
+	}
+  }
+
+	  	.second-section::after {
+		  content: '';
+		  display: block;
+		  width: 0px;
+		  height: 0px;
+		  border-top: solid 4rem RGBA(245, 245, 245, 1.00);
+		  border-left: solid 5rem #FFF;
+		  border-right: solid 5rem #FFF;
+		  position: absolute;
+		  bottom: 4.3rem;
+		  left: 50%;
+		  margin-left: -5rem;
+		  z-index: 10;
+
+		}
 
 </style>
